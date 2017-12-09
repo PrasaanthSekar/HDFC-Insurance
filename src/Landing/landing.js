2965f156198b4cdf4af85afa491c015bb3b8a5f3
M1032467 <M1032467@mindtree.com>
2017-12-10 01:35:37 +0530

    Complete landing page

--- src/Landing/landing.js
+++ src/Landing/landing.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import Paper from 'material-ui/Paper';
+
+import './landing.css';
+
+const style = {
+    height: 100,
+    width: 100,
+    margin: 30,
+    textAlign: 'center',
+    display: 'inline-block',
+};
 
 class Landing extends Component {
     render() {
         return (
             <div className="Container">
+                <div className="banner">
+                    <div className="banner-text">
+                        <span className="banner-heading">Insurance </span><br />
+                        <span className="banner-heading">Hackathon</span>
 
+                        <span className="banner-content">Chat with our new interative bot to know more about our insurance policies with our new interactive experience. Click on try now to enter an all new experience</span>
+                        <FlatButton className="btn-color" label="Try now" />
+                    </div>
+                </div>
+                <div class="main">
+                    <Paper className="avatar-left" style={style} zDepth={2} circle={true} />
+                    <div class="sub">
+                        <div class="font">
+                            "We chose Linkerd as the service mesh for Apprenda's Kismatic Toolkit because it brings best-in-breed resilience and visibility to cloud-native applications."
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="main-right">
+                    <Paper className="avatar-right" style={style} zDepth={2} circle={true} />
+                    <div class="sub-right">
+                        <div class="font">
+                            "We chose Linkerd as the service mesh for Apprenda's Kismatic Toolkit because it brings best-in-breed resilience and visibility to cloud-native applications."
+                        </div>
+                    </div>
+                </div>
+                <div className="pre-footer">
+                    <center className="pre-footer-text">Get started today</center><br />
+                    <center><FlatButton className="footer-btn" label="Try now" /></center>
+                </div>
+                <div className="footer-text">
+                    <span className="footer">Terms and Privacy | Third-Party Licenses</span>
+                </div>
             </div>
         );
     }
