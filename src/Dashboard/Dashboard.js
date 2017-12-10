b02d8129755c64cc7a5fdbeb719af561ec347ad3
rajendra prasad <rajendraprasad.jeevanantham@mindtree.com>
2017-12-10 10:12:51 +0530

    Fixes

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 import './animate.min.css';
 import single from './images/single.png';
 import women from './images/women.png';
-// import marriedCouple from './images/marriedCouple.png';
+import marriedCouple from './images/marriedCouple.png';
 import marriedCoupleWithChildren from './images/marriedcouplewithchildren.png';
 import marriedcouplewithhouseloan from './images/marriedcouplewithhouseloan.png'
 import './index.css'
@@ -17,12 +17,12 @@ class DashBoard extends Component {
     let display = null;
     if (parameters !== null) {
       if (parameters.gender === "male") {
-        display = <img src={single} className="animated bounce" />
+        display = <img src={single} className="animated bounce cartoon_img" />
       } else if (parameters.gender === "female") {
-        display = <div className='singlefemale'></div>
+        display = <img src={women} className="animated bounce cartoon_img" />
       }
-      if (parameters.status !== "Children" && parameters.Loanstatus !== "Loan" && parameters.status === "married") {
-        display = <div className='marriedCouple'></div>
+      if(parameters.status === "married"){
+        display = <img src={marriedCouple} className="animated bounce cartoon_img" />
       }
     }
 
